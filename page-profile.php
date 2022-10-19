55eb553c0a204604da9b2ffae5bec05539727d67
unknown <flaming.june123@gmail.com>
2022-10-20 07:14:15 +0900

    20221020_0714

--- page-profile.php
+++ page-profile.php
@@ -1,53 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>narumiのPortfolio</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS -->
-        <link rel="stylesheet" href="https://unpkg.com/destyle.css@1.0.5/destyle.css">
-        <link href="./css/style.css" rel="stylesheet">
-    <!--Googlefont-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;600;700&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-    <!--jQuery-->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <!-- Font Awesome -->
-        <script src="https://kit.fontawesome.com/b8a7fea4d4.js"></script>
-    </head>
-	<body>
-        <header class="l-header--lower-page">
-            <div class="p-header__nav__wrapper">
-                <p class="p-nav--btn">
-                    <a href="javascript:void(0)"><span>メニューを開く</span></a>
-                </p>
-                <nav class="p-header__nav">
-                    <ul class="p-header__nav__main-menu">
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--home" href="#">Home</a></li>
-                        <li class="p-header__nav__main-menu__item--other"><a class="p-header__nav__main-menu__item--contact" href="#">Contact</a></li>
-                    </ul>
-                </nav>
-                <!--SP用のnavメニュー-->
-                <nav class="p-header__nav--sp">
-                    <ul class="p-header__nav__main-menu--sp">
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--profile" href="#">Top</a></li>
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--profile" href="#">Profile</a></li>
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--skill" href="#">Skill</a></li>
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--works" href="#">Works</a></li>
-                        <li class="p-header__nav__main-menu__item--other"><a class="p-header__nav__main-menu__item--contact" href="#">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="p-header__title__wrapper">
-                <p class="p-header__title">narumiのポートフォリオ</p>
-                <div class="p-header__breadcrumb__wrapper">
-                    <a class="p-header__breadcrumb__home" href="#">Home</a>
-                    <div class="p-header__breadcrumb__arrow">&gt;</div>
-                    <div class="p-header__breadcrumb__current">Works</div>
-                </div>
-            </div>
-        </header>
+<?php
+/*
+Template Name:profile
+*/
+?>
+<?php get_header(); ?>
         <main class="p-main--lower-page">
             <div class="p-profile__container--lower-page">
                 <!--中央部分のwrapper-->
@@ -192,12 +148,5 @@
                 <div class="p-lower-page__side--right"></div>
             </div>
         </main>
-        <footer class="l-footer">
-            <div class="p-footer__copyright">
-                <p class="p-footer__copyright__text">copyright&nbsp;&copy;narumi,All&nbsp;rights&nbsp;reserved.</p>
-            </div>
-        </footer>
-    <script src="js/on.click.js"></script>
-    <script src="js/fade-in-out.js"></script>
-	</body>
+        <?php get_footer(); ?>
 </html>
