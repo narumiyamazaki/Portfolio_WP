2815601355b2e3e5be7d77acbea530204acbf898
unknown <flaming.june123@gmail.com>
2022-10-26 07:23:31 +0900

    20221026_0723

---
+++ archive.php
@@ -0,0 +1,50 @@
+<article class="p-works">
+                <!--中央部分のcontainer-->
+                <div class="p-works__container">
+                    <h2 class="p-works__heading2--English">Works<span class="p-works__heading2--Japanese">作品事例</span></h2>
+                    <p class="p-works__text">今までの制作事例です</p>
+                    <!--投稿を取得する。カスタム投稿タイプを使って、ループで記事を取得するのでは？-->    
+                    <div class="p-works__card__container--1st">
+                        <article class="p-works__card__wrapper">
+                            <a class="p-works__card" href="#">
+                                <div class="p-works__card__image--portfolio"><span class="c-works__card__role">Design&amp;Coding</span></div>
+                                <h3 class="p-works__card__caption">ポートフォリオサイト</h3>
+                            </a>
+                        </article>
+                        <article class="p-works__card__wrapper">
+                            <a class="p-works__card" href="https://raisetech-hamburger.com/">
+                                <div class="p-works__card__image--hamburger"><span class="c-works__card__role">Coding</span></div>
+                                <h3 class="p-works__card__caption">スクール課題<br>ハンバーガーショップサイト</h3>
+                            </a>
+                        </article>
+                        <article class="p-works__card__wrapper">
+                            <a class="p-works__card" href="https://narumiyamazaki.github.io/Portfolio__demo-web-production-company_open/">
+                                <div class="p-works__card__image--web-production-company"><span class="c-works__card__role">Coding</span></div>
+                                <h3 class="p-works__card__caption">デモサイト<br>Web制作会社コーポレートサイト</h3>
+                            </a>
+                        </article>
+                    </div>
+                    <div class="p-works__card__container--2nd">
+                        <article class="p-works__card__wrapper">
+                            <a class="p-works__card" href="">
+                                <div class="p-works__card__image--RaiseTech-web-banner"><span class="c-works__card__role">Design</span></div>
+                                <h3 class="p-works__card__caption">スクール課題<br>RaiseTech&nbsp;Web出稿バナー</h3>
+                            </a>
+                        </article>
+                        <article class="p-works__card__wrapper">
+                            <a class="p-works__card" href="">
+                                <div class="p-works__card__image--RaiseTech-sns-banner"><span class="c-works__card__role">Design</span></div>
+                                <h3 class="p-works__card__caption">スクール課題<br>RaiseTech&nbsp;インスタ出稿バナー</h3>
+                            </a>
+                        </article>
+                        <article class="p-works__card__wrapper">
+                            <a class="p-works__card" href="">
+                                <div class="p-works__card__image--miraini-logo"><span class="c-works__card__role">Design</span></div>
+                                <h3 class="p-works__card__caption">スクール課題<br>調剤薬局ロゴ</h3>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+                <div class="p-top-page__side--left"><span class="c-side__text">profile</span></div>
+                <div class="p-top-page__side--right"><span class="c-side__text">profile</span></div>
+            </article>
