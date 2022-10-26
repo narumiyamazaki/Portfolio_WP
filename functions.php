33f92f501d9a9ba476b60d4db70bb1e50da86f32
unknown <flaming.june123@gmail.com>
2022-10-27 07:11:22 +0900

    20221027_0711

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ function create_post_type() {
   register_post_type( // カスタム投稿タイプの追加関数
     'news', //カスタム投稿タイプ名（半角英数字の小文字）
     array( //オプション（以下）
-      'label' => 'Works', // 管理画面上の表示（日本語でもOK）
+      'label' => 'news', // 管理画面上の表示（日本語でもOK）
       'public' => true, // 管理画面に表示するかどうかの指定
       'has_archive' => true, // 投稿した記事の一覧ページを作成する
       'menu_position' => 5, // 管理画面メニューの表示位置（投稿の下に追加）
@@ -67,10 +67,26 @@ function create_post_type() {
       ),
     )
   );
-    // 「ニュース」のカスタム投稿にカテゴリーを追加
+  register_post_type(
+    'works', //カスタム投稿タイプ名
+    array( //オプション
+      'works' => 'サービス紹介',
+      'public' => true,
+      'has_archive' => true,
+      'menu_position' => 5,
+      'show_in_rest' => true,
+      'supports' => array(
+          'title',  // タイトル
+          'editor', // エディター
+          'thumbnail', // アイキャッチ画像
+          'revisions' // リビジョンの保存
+      ),
+    )
+  );
+    // 「works」のカスタム投稿にカテゴリーを追加
     register_taxonomy(
-        'news-cat',
-        'news', // カテゴリーを追加したいカスタム投稿タイプ名
+        'works-cat',
+        'works', // カテゴリーを追加したいカスタム投稿タイプ名
         array(
           'label' => 'カテゴリー',
           'hierarchical' => true,
