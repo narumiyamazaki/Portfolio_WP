8675ab27faa7058452acd3e8039324e8cf1ce19b
naruiyamazaki <flaming.june123@gmail.com>
2022-10-27 16:18:50 +0900

    20221027_1618

--- functions.php
+++ functions.php
@@ -52,9 +52,9 @@ add_filter('walker_nav_menu_start_el', 'add_class_on_link', 10, 4);
 /* ---------- カスタム投稿の追加 ---------- */
 function create_post_type() {
   register_post_type( // カスタム投稿タイプの追加関数
-    'news', //カスタム投稿タイプ名（半角英数字の小文字）
+    'works', //カスタム投稿タイプ名（半角英数字の小文字）
     array( //オプション（以下）
-      'label' => 'news', // 管理画面上の表示（日本語でもOK）
+      'label' => 'works', // 管理画面上の表示（日本語でもOK）
       'public' => true, // 管理画面に表示するかどうかの指定
       'has_archive' => true, // 投稿した記事の一覧ページを作成する
       'menu_position' => 5, // 管理画面メニューの表示位置（投稿の下に追加）
@@ -64,22 +64,6 @@ function create_post_type() {
         'editor', // エディター
         'thumbnail', // アイキャッチ画像
         'revisions' // リビジョンの保存
-      ),
-    )
-  );
-  register_post_type(
-    'works', //カスタム投稿タイプ名
-    array( //オプション
-      'works' => 'サービス紹介',
-      'public' => true,
-      'has_archive' => true,
-      'menu_position' => 5,
-      'show_in_rest' => true,
-      'supports' => array(
-          'title',  // タイトル
-          'editor', // エディター
-          'thumbnail', // アイキャッチ画像
-          'revisions' // リビジョンの保存
       ),
     )
   );
