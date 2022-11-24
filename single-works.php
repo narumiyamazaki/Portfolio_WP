36cdb1868a3b65c3cec7df8cceffeb02489815a7
naruiyamazaki <flaming.june123@gmail.com>
2022-11-24 16:14:20 +0900

    20221124_1614

--- single-works.php
+++ single-works.php
@@ -49,21 +49,25 @@
                     //本文記事の取得
                     $content_string = get_the_content();
                     //置き換え対象の指定
-                    $target = array('<p>',
+                    $target = array('<!-- wp:paragraph -->',
+                                    '<p>',
                                     'wp-block-quote',
                                     '・水色・青・黄緑、自然で清潔なイメージ<br>・誠実、やさしさ、ホスピタリティ、すっきりとしたデザイン<br>・よくある「太めの丸ゴシック」にはしたくない。',
-                                    //'・水色・青・黄緑、自然で清潔なイメージ<br>・誠実、やさしさ、ホスピタリティ、すっきりとしたデザイン<br>・よくある「太めの丸ゴシック」にはしたくない。<br><br>',
+                                    '・水色・青・黄緑、自然で清潔なイメージ<br>・誠実、やさしさ、ホスピタリティ、すっきりとしたデザイン<br>・よくある「太めの丸ゴシック」にはしたくない。<br><br>',
                                     '<p class="p-works__content__text">ミライニは、「明るい未来に向けて薬局としての役割を果たす」という「未来に」を、読みやすさとすっきりとした文字のカタカナで表現。</p>',
-                                    '<p class="p-works__content__text">病院の先生とは違う、薬の専門家として「薬のことを相談しやすい」「気軽に質問できる」ホスピタリティを目指す。</p>');
+                                    '<p class="p-works__content__text">病院の先生とは違う、薬の専門家として「薬のことを相談しやすい」「気軽に質問できる」ホスピタリティを目指す。</p>',
+                                    '<!-- /wp:paragraph -->');
                     //置き換え後の文字
-                    $replace = array('<p class="p-works__content__text"><span class="u-margin-16px"></span>',
+                    $replace = array('',
+                                    '<p class="p-works__content__text"><span class="u-margin-16px"></span>',
                                     'p-quote__wrapper',
                                     '<ul class="p-quote__list__wrapper"><li class="p-quote__list">・水色・青・黄緑、自然で清潔なイメージ</li><li class="p-quote__list">・誠実、やさしさ、ホスピタリティ、すっきりとしたデザイン</li><li class="p-quote__list">・よくある&#12300;太めの丸ゴシック&#12301;にはしたくない。</li></ul>',
-                                    //'<ul class="p-quote__list__wrapper"><li class="p-quote__list">・水色・青・黄緑、自然で清潔なイメージ</li><li class="p-quote__list">・誠実、やさしさ、ホスピタリティ、すっきりとしたデザイン</li><li class="p-quote__list">・よくある&#12300;太めの丸ゴシック&#12301;にはしたくない。</li></ul>',
+                                    '<ul class="p-quote__list__wrapper"><li class="p-quote__list">・水色・青・黄緑、自然で清潔なイメージ</li><li class="p-quote__list">・誠実、やさしさ、ホスピタリティ、すっきりとしたデザイン</li><li class="p-quote__list">・よくある&#12300;太めの丸ゴシック&#12301;にはしたくない。</li></ul>',
                                     '<p class="p-quote__text"><span class="u-margin-16px"></span>ミライニは、&#12300;明るい未来に向けて薬局としての役割を果たす&#12301;という&#12300;未来に&#12301;を、読みやすさとすっきりとした文字のカタカナで表現。</p>',
-                                    '<p class="p-quote__text--last">病院の先生とは違う、薬の専門家として&#12300;薬のことを相談しやすい&#12301;&#12300;気軽に質問できる&#12301;ホスピタリティを目指す。</p>');
+                                    '<p class="p-quote__text--last">病院の先生とは違う、薬の専門家として&#12300;薬のことを相談しやすい&#12301;&#12300;気軽に質問できる&#12301;ホスピタリティを目指す。</p>',
+                                    '');
                     $content_string = str_replace($target,$replace,$content_string); 
-                    echo esc_html($content_string);
+                    echo $content_string;
                 ?>
             </div>
         </article>
