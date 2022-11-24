aa23c052aa697140973c954e447217fb21e23a14
unknown <flaming.june123@gmail.com>
2022-11-25 06:11:44 +0900

    20221125_0611

--- header.php
+++ header.php
@@ -4,13 +4,6 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS 
-        <link rel="stylesheet" href="https://unpkg.com/destyle.css@1.0.5/destyle.css">
-    -->
-    <!--Googlefont
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-    -->
     <?php wp_head(); ?>
     </head>
 	<body <?php body_class(); ?>>
@@ -38,16 +31,5 @@
                         'add_li_class' => 'p-header__nav__main-menu__item',
                     ));
                 ?>
-                <!--SP用のnavメニュー
-                <nav class="p-header__nav--sp">
-                    <ul class="p-header__nav__main-menu--sp">
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--profile" href="#">Top</a></li>
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--profile" href="#">Profile</a></li>
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--skill" href="#">Skill</a></li>
-                        <li class="p-header__nav__main-menu__item"><a class="p-header__nav__main-menu__item--works" href="#">Works</a></li>
-                        <li class="p-header__nav__main-menu__item--other"><a class="p-header__nav__main-menu__item--contact" href="#">Contact</a></li>
-                    </ul>
-                </nav>
-                -->
             </div>
         </header>
